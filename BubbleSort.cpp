8deb6318c82ca07e71093d57f6843c69a8473891
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-09-24 07:50:30 -0700

    no changes

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 void BubbleSort(int arr[] , int size){
+
     for(int i = 0; i<size-1;i++){
         for(int j = 0; j<size-i-1; j++){
             if(arr[j] > arr[j+1]){
                 //swaping process
                 swap(arr[j] , arr[j+1]);
+
             }
         }
+        
     }
 }
 
@@ -18,7 +21,7 @@ void Print_array(int arr[] , int size){
      }
 }
 int main(){
-    int arr[5] = {4,2,6,3,8};
+    int arr[5] = {1,2,3,4,5};
     int size = 5;
 
     BubbleSort(arr,size);
