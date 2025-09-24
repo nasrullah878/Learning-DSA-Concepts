0b2f3908f47f80a34d5f0ac8cb93cb640cd7f1ae
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-09-24 08:13:25 -0700

    Bubble sort Decending order

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,7 +5,7 @@ void BubbleSort(int arr[] , int size){
 
     for(int i = 0; i<size-1;i++){
         for(int j = 0; j<size-i-1; j++){
-            if(arr[j] > arr[j+1]){
+            if(arr[j] < arr[j+1]){
                 //swaping process
                 swap(arr[j] , arr[j+1]);
 
